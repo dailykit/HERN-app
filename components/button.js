dcf01298d214e6882044d8016563506f71d89d8b
rahul72925 <rahulkhatri72925@gmail.com>
2022-06-16 13:42:59 +0530

    feat: button variant

--- components/button.js
+++ components/button.js
@@ -1,4 +1,6 @@
-import { TouchableOpacity, StyleSheet, Text } from 'react-native'
+import { FascinateInline_400Regular } from '@expo-google-fonts/dev'
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native'
+import RadioIcon from '../assets/radioIcon'
 import appConfig from '../brandConfig.json'
 
 export const Button = ({
@@ -6,28 +8,76 @@ export const Button = ({
    onPress,
    buttonStyle = null,
    textStyle = null,
+   variant = 'primary',
+   isActive = false,
+   showRadio = false,
+   radioSize = 16,
 }) => {
+   const { buttonSettings } = appConfig.brandSettings
+   const containerStyleByVariant = variant => {
+      switch (variant) {
+         case 'primary':
+            return {
+               backgroundColor: buttonSettings.buttonBGColor.value || '#000000',
+            }
+         case 'outline':
+            return {
+               borderColor: isActive
+                  ? buttonSettings.borderActiveColor.value
+                  : buttonSettings.borderInactiveColor.value,
+               borderWidth: 1,
+               backgroundColor: '#FFFFFF',
+            }
+      }
+   }
+   const textStyleByVariant = variant => {
+      switch (variant) {
+         case 'primary':
+            return {
+               color: buttonSettings.textColor.value || '#ffffff',
+            }
+         case 'outline':
+            return { color: '#000000' }
+      }
+   }
    return (
       <TouchableOpacity
          onPress={onPress}
          style={[
             styles.buttonContainerStyle,
-            {
-               backgroundColor:
-                  appConfig.brandSettings.buttonSettings.buttonBGColor.value ||
-                  '#000000',
-            },
+            containerStyleByVariant(variant),
             buttonStyle ? buttonStyle : null,
+            {
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center',
+               flexDirection: 'row',
+            },
          ]}
       >
+         {showRadio ? (
+            <View
+               style={{
+                  paddingLeft: 12,
+               }}
+            >
+               <RadioIcon
+                  checked={isActive}
+                  size={radioSize}
+                  stroke={
+                     isActive
+                        ? appConfig.brandSettings.checkIconSettings
+                             .checkIconFillColor.value
+                        : appConfig.brandSettings.checkIconSettings
+                             .boundaryColor.value
+                  }
+               />
+            </View>
+         ) : null}
          <Text
             style={[
                styles.buttonTextStyle,
-               {
-                  color:
-                     appConfig.brandSettings.buttonSettings.textColor.value ||
-                     '#ffffff',
-               },
+               textStyleByVariant(variant),
                textStyle ? textStyle : null,
             ]}
          >
