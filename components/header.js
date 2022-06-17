f768320214809f8dbf203082a94c078c0226f603
rahul72925 <rahulkhatri72925@gmail.com>
2022-06-17 16:12:39 +0530

    feat: fulfillment details on header

--- components/header.js
+++ components/header.js
@@ -3,9 +3,88 @@ import CartIcon from '../assets/cartIcon'
 import LocationIcon from '../assets/locationIcon'
 import appConfig from '../brandConfig.json'
 import { useNavigation } from '@react-navigation/native'
+import { useConfig } from '../lib/config'
+import React from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const Header = () => {
    const navigation = useNavigation()
+   const { dispatch, orderTabs, selectedOrderTab, userLocation, storeStatus } =
+      useConfig()
+   React.useEffect(() => {
+      if (orderTabs?.length > 0) {
+         ;(async function () {
+            const orderTab = await AsyncStorage.getItem('orderTab')
+            const storeLocationId = await AsyncStorage.getItem(
+               'storeLocationId'
+            )
+            const userLocation = await AsyncStorage.getItem('userLocation')
+            const storeLocation = await AsyncStorage.getItem('storeLocation')
+            if (storeLocationId) {
+               dispatch({
+                  type: 'SET_STORE_LOCATION_ID',
+                  payload: storeLocationId,
+               })
+               const selectedOrderTab = orderTabs.find(
+                  x => x.orderFulfillmentTypeLabel == orderTab
+               )
+
+               dispatch({
+                  type: 'SET_SELECTED_ORDER_TAB',
+                  payload: selectedOrderTab,
+               })
+               dispatch({
+                  type: 'SET_USER_LOCATION',
+                  payload:
+                     orderTab == 'ONDEMAND_DELIVERY' ||
+                     orderTab == 'PREORDER_DELIVERY'
+                        ? JSON.parse(userLocation)
+                        : JSON.parse(storeLocation),
+               })
+               dispatch({
+                  type: 'SET_STORE_STATUS',
+                  payload: {
+                     status: true,
+                     message: 'Store available on your location.',
+                     loading: false,
+                  },
+               })
+            } else {
+               dispatch({
+                  type: 'SET_STORE_STATUS',
+                  payload: {
+                     status: false,
+                     message: 'Select your location',
+                     loading: false,
+                  },
+               })
+            }
+         })()
+      }
+   }, [orderTabs])
+
+   const label = React.useMemo(() => {
+      if (selectedOrderTab?.orderFulfillmentTypeLabel) {
+         const fulfillmentType = selectedOrderTab.orderFulfillmentTypeLabel
+         switch (fulfillmentType) {
+            case 'ONDEMAND_DELIVERY':
+               return 'Delivery At'
+            case 'PREORDER_DELIVERY':
+               return 'Delivery At'
+            case 'PREORDER_PICKUP':
+               return 'Pickup From'
+            case 'ONDEMAND_PICKUP':
+               return 'Pickup From'
+            case 'PREORDER_DINEIN':
+               return 'Dine In At'
+            case 'ONDEMAND_DINEIN':
+               return 'Dine In At'
+         }
+      } else {
+         return null
+      }
+   }, [selectedOrderTab])
+
    return (
       <View
          style={[
@@ -24,18 +103,56 @@ export const Header = () => {
             }}
          >
             <LocationIcon />
-            <Text
-               style={[
-                  styles.headerTextStyle,
-                  {
-                     color:
-                        appConfig.brandSettings.headerSettings?.textColor
-                           ?.value || '#000000',
-                  },
-               ]}
-            >
-               Select Your location
-            </Text>
+            <View>
+               {storeStatus.loading ? (
+                  <Text
+                     style={{
+                        color:
+                           appConfig.brandSettings.headerSettings?.textColor
+                              ?.value || '#000000',
+                        fontStyle: 'italic',
+                     }}
+                  >
+                     Getting your location
+                  </Text>
+               ) : storeStatus.status ? (
+                  <View>
+                     <Text
+                        style={{
+                           color:
+                              appConfig.brandSettings.headerSettings?.textColor
+                                 ?.value || '#000000',
+                        }}
+                     >
+                        {label}
+                     </Text>
+                     <Text
+                        style={{
+                           color:
+                              appConfig.brandSettings.headerSettings?.textColor
+                                 ?.value || '#000000',
+                        }}
+                     >
+                        {userLocation?.label
+                           ? userLocation?.label
+                           : userLocation?.line1}
+                     </Text>
+                  </View>
+               ) : (
+                  <Text
+                     style={[
+                        styles.headerTextStyle,
+                        {
+                           color:
+                              appConfig.brandSettings.headerSettings?.textColor
+                                 ?.value || '#000000',
+                        },
+                     ]}
+                  >
+                     Select Your location
+                  </Text>
+               )}
+            </View>
          </TouchableOpacity>
          <TouchableOpacity>
             <Text
