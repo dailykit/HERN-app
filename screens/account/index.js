fd94799dda085e1bd77b320d4f0d5fd5f78f2ad9
rahul72925 <rahulkhatri72925@gmail.com>
2022-06-24 11:22:35 +0530

    feat: user info on account screen

--- screens/account/index.js
+++ screens/account/index.js
@@ -1,7 +1,16 @@
-import { Text } from 'react-native'
+import { View } from 'react-native'
+import { AccountHeader } from './header'
+import { UserInfo } from './userInfo'
 
 const AccountScreen = () => {
-   return <Text>Account</Text>
+   return (
+      <View>
+         <AccountHeader />
+         <View style={{ marginHorizontal: 12 }}>
+            <UserInfo />
+         </View>
+      </View>
+   )
 }
 
 export default AccountScreen
